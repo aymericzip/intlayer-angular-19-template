a03634ce54b9cf8630fe992a763aaddc17e45fdc
aymericzip <ay.pineau@gmail.com>
2026-08-11 15:39:32 -0600

    chore: downgrade Angular dependencies and update TypeScript version

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,11 @@
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRouting } from '@angular/ssr';
 import { type ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
+  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
